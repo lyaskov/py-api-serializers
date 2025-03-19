2c064f56de1c267eee68c3cc8715ac10ca687bf6
lyaskov <lyaskov1@gmail.com>
2025-03-19 18:49:47 +0200

    Remove django-debug-toolbar

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "debug_toolbar",
     "rest_framework",
     "cinema",
     "user",
@@ -53,7 +52,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "cinema_service.urls"
@@ -76,9 +74,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "cinema_service.wsgi.application"
 
-INTERNAL_IPS = [
-    "127.0.0.1",
-]
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
